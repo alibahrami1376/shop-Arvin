bcfabe0214e690ca80af916696c3fd5eba7a08d3
YourName <alifbahrami13766@gmail.com>
2026-02-25 14:47:48 +0330

    feat:add redirect url for loin and logout

--- core/core/settings.py
+++ core/core/settings.py
@@ -152,3 +152,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.User'
 
 MERCHANT_ID = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+
+LOGOUT_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "/"
--- core/website/admin.py
+++ core/website/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from website.models import ContactModel
 # Register your models here.
+@admin.register(ContactModel)
+class OrderModelAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "full_name",
+        "email",
+        "phone_number",
+        "subject",
+        "content",
+        "is_seen",
+        "created_date",
+
+    )
