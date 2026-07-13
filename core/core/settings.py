865e1bf5357d0f3b99815b673280e539518191ec
YourName <alifbahrami13766@gmail.com>
2026-07-13 11:41:16 +0330

    fix:add setting for mount disk to one folder in runflare

--- core/core/settings.py
+++ core/core/settings.py
@@ -149,16 +149,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
 
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATIC_ROOT = "/app/public/static"
+MEDIA_ROOT = "/app/public/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
