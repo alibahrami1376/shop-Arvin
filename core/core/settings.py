6dddb24efeb00b0b613ff1d29e3c65e40232b055
YourName <alifbahrami13766@gmail.com>
2026-07-18 17:49:57 +0330

    fix: add missing static url configuration

--- core/core/settings.py
+++ core/core/settings.py
@@ -146,8 +146,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = config("STATIC_ROOT")
-MEDIA_ROOT = config("MEDIA_ROOT")
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
+
+STATIC_ROOT = config("STATIC_ROOT", default="/app/public/static")
+MEDIA_ROOT = config("MEDIA_ROOT", default="/app/public/media")
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
