8343a2ce95fbc5abec0808e6202528d564fa6810
YourName <alifbahrami13766@gmail.com>
2026-07-23 20:31:57 +0330

    fix: order header parent categories by id

--- core/shop/context_processors.py
+++ core/shop/context_processors.py
@@ -8,5 +8,5 @@ def shop_categories(request):
     return {
         "shop_categories": ProductCategoryModel.objects.filter(parent__isnull=True)
         .prefetch_related(Prefetch("children", queryset=children_qs))
-        .order_by("title"),
+        .order_by("id"),
     }
